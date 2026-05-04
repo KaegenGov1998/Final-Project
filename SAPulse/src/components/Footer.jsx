c2982b10edd425559f93993880c1885a93ac8fac
KaegenGov1998 <152480939+KaegenGov1998@users.noreply.github.com>
2026-05-04 21:51:03 +0200

    Footer added and admin finished v1

---
+++ SAPulse/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className='w-full flex flex-row border-t-2 border-gray-200 justify-between font-light'>
+        <p>©SAPulse 2026</p>
+        <p>Terms and Conditions</p>
+
+      </div>
+    </>
+  )
+}
+
+export default Footer
--- SAPulse/src/pages/Admin.jsx
+++ SAPulse/src/pages/Admin.jsx
@@ -1,43 +1,60 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import { Search } from "lucide-react";
 
 const admin = ({ token }) => {
-
   return (
     <>
-      <div className='flex flex-row bg-gray-50'>
+      <div className="flex flex-row bg-gray-50 h-screen">
         <Sidebar />
-        <div className='flex flex-col w-full'>
-          <h1>Welcome back, Admin {token.user.user_metadata.full_name}</h1>
-          <p>Manage members of the app and control their levels of access</p>
+        <div className="flex flex-col w-full gap-4 pr-4 pt-8 justify-around">
           <div>
-            <div className='bg-white border-2 border-gray-100 p-2 rounded-md w-full'>
-              <table class="table-auto" className='w-full'>
-                <thead className='border-b-2 border-gray-100'>
+          <h1 className="text-4xl font-bold">
+            Welcome back, Admin {token.user.user_metadata.full_name}
+          </h1>
+          <div>
+            <p className="text-lg font-light">
+              Manage members of the app and control their levels of access
+            </p>
+            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row w-120 bg-white border-2 border-gray-100 rounded-md p-2 my-2">
+              <Search className="mr-2"/>
+              <input placeholder="search" />
+            </div>
+            <button className="bg-blue-950 rounded-md p-2 text-white cursor-pointer">
+              + Add Member
+            </button>
+          </div>
+            <div className="bg-white border-2 border-gray-100 p-2 rounded-md w-full">
+              <table class="table-auto" className="w-full table-fixed border-separate border-spacing-y-6">
+                <thead className="border-b-2 border-gray-100">
                   <tr>
-                    <th className='w-1/4 text-xl font-bold'>Name</th>
-                    <th className='w-1/4 text-xl font-bold'>Location</th>
-                    <th className='w-1/4 text-xl font-bold'>Leadership Status</th>
-                    <th className='w-1/4 text-xl font-bold'>Admin Status</th>
+                    <th className="w-1/4 text-xl font-bold">Name</th>
+                    <th className="w-1/4 text-xl font-bold">Location</th>
+                    <th className="w-1/4 text-xl font-bold">
+                      Leadership Status
+                    </th>
+                    <th className="w-1/4 text-xl font-bold">Admin Status</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td className='font-medium'>Kaegen Govender</td>
-                    <td>Johannesburg</td>
+                    <td className="font-medium">Kaegen Govender</td>
+                    <td className="font-light">Johannesburg</td>
                     <td>Yes</td>
                     <td>Yes</td>
                   </tr>
                   <tr>
-                    <td className='font-medium'>Harry Potter</td>
-                    <td>Hogwarts</td>
+                    <td className="font-medium">Harry Potter</td>
+                    <td className="font-light">Hogwarts</td>
                     <td>Yes</td>
                     <td>No</td>
                   </tr>
                   <tr>
-                    <td className='font-medium'>Jasmine Kennedie</td>
-                    <td>New York</td>
+                    <td className="font-medium">Jasmine Kennedie</td>
+                    <td className="font-light">New York</td>
                     <td>No</td>
                     <td>No</td>
                   </tr>
@@ -45,10 +62,12 @@ const admin = ({ token }) => {
               </table>
             </div>
           </div>
+          </div>
+          <Footer/>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default admin
+export default admin;
