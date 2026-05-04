82f18c76496e5836514eee6366cfff1cfca1166e
KaegenGov1998 <152480939+KaegenGov1998@users.noreply.github.com>
2026-05-04 21:54:09 +0200

    adjusted footer

--- SAPulse/src/components/Footer.jsx
+++ SAPulse/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div className='w-full flex flex-row border-t-2 border-gray-200 justify-between font-light'>
+      <div className='w-full flex flex-row border-t-2 border-gray-200 justify-between font-light pt-4'>
         <p>©SAPulse 2026</p>
         <p>Terms and Conditions</p>
 
