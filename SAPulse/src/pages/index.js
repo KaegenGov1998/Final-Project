68ebee07c6930468c4e97a614ca038c4d5937bb3
KaegenGov1998 <152480939+KaegenGov1998@users.noreply.github.com>
2026-05-18 13:25:23 +0200

    updated index.js

--- SAPulse/src/pages/index.js
+++ SAPulse/src/pages/index.js
@@ -1,9 +1,9 @@
-import Login from './Login'
-import SignUp from './SignUp'
-import Homepage from './Homepage'
-import Admin from './Admin'
-import Post from './Post'
-import NotAdminPage from './NotAdminPage'
-import NotLeader from './NotLeader'
+import Login from './Login.jsx'
+import SignUp from './SignUp.jsx'
+import Homepage from './Homepage.jsx'
+import Admin from './Admin.jsx'
+import Post from './Post.jsx'
+import NotAdminPage from './NotAdminPage.jsx'
+import NotLeader from './NotLeader.jsx'
 
-export { Login, SignUp, Homepage, Admin, NotAdminPage, Post, NotLeader}
+export { Login, SignUp, Homepage, Admin, NotAdminPage, Post, NotLeader }
